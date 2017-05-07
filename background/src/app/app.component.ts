fae389ec853bd7aa4950ef803291fbe5c92a74db
kranthi veeramachaneni <kranthi1056@gmail.com>
2017-05-07 14:50:32 +0100

    added bootstrap to background

--- background/src/app/app.component.ts
+++ background/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import * as tether from 'tether';
+import * as bootstrap from 'bootstrap';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
