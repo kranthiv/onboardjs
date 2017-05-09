539a528704e909c23bba906b8af27fad82147a71
kranthi veeramachaneni <kranthi1056@gmail.com>
2017-05-09 23:34:07 +0100

    add stuff for background

---
+++ background/webpack.config.js
@@ -0,0 +1,27 @@
+let path = require('path');
+let webpack = require('webpack');
+
+module.exports={
+    entry:'./src/index.ts',
+    output:{
+        filename:'background.js',
+        path:__dirname + '/dist'
+    },
+    devtool:'source-map',
+    resolve:{
+        extensions:['.ts','.js']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.tsx?$/,
+                loader:'ts-loader'
+            },
+            {
+                enforce:'pre',
+                test:/\.js$/,
+                loader:'source-map-loader'
+            }
+        ]
+    }
+}
