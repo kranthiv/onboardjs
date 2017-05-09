8590d2a7d637976a7146eb6f73936728e7a23c00
kranthi veeramachaneni <kranthi1056@gmail.com>
2017-05-09 23:58:09 +0100

    removed unwanted files

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(() => {
-    chrome.tabs.create({ 'url': chrome.runtime.getURL('background/dist/index.html') }, function (tab) {
-        console.log("tab opened", tab);
-    });
-});
